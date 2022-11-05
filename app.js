7ff0143e18dfbefef12630ce21192fcc02aeb8b7
kannan-c1609 <67515928+kannan-c1609@users.noreply.github.com>
2022-11-05 11:12:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const selectBtn = document.querySelector(".select-btn");
 const items1 = document.getElementsByClassName("items");
-const items2 = document.getElementsByClassName("items2");
+
+let totalPlayer = 0
+let myPlayers = [];
+
 
 selectBtn.addEventListener("click", ()=> {
     selectBtn.classList.toggle("open");
@@ -13,28 +16,41 @@ Array.from(items1).forEach(item => {
         let checked = document.querySelectorAll(".checked"),
             btnText = document.querySelector(".btn-text");
 
-            if(checked && checked.length > 0) {
+            if(checked && checked.length > 0 && checked.length <= 7) {
                 btnText.innerText = `${checked.length} selected`;
+            }
+            else if(checked.length > 7) {
+                item.classList.toggle("checked");
             }
             else {
                 btnText.innerText = `Team 1 - Select Players`
+            }
+            if(checked) {
+                
             }
         console.log(checked.length)
     })
 })
 
-Array.from(items2).forEach(item => {
+const items11 = document.getElementsByClassName("items1");
+const selectBtn1 = document.querySelector(".select-btn1");
+
+selectBtn1.addEventListener("click", ()=> {
+    selectBtn1.classList.toggle("open");
+});
+
+Array.from(items11).forEach(item => {
     item.addEventListener("click", () => {
-        item.classList.toggle("checked");
+        item.classList.toggle("checked1");
 
-        let checked = document.querySelectorAll(".checked"),
-            btnText = document.querySelector(".btn-text");
+        let checked1 = document.querySelectorAll(".checked1"),
+            btnText1 = document.querySelector(".btn-text1");
 
-            if(checked && checked.length > 0) {
-                btnText.innerText = `${checked.length} selected`;
+            if(checked1 && checked1.length > 0) {
+                btnText1.innerText = `${checked1.length} selected`;
             }
             else {
-                btnText.innerText = `Team 1 - Select Players`
+                btnText1.innerText = `Team 2 - Select Players`
             }
         console.log(checked.length)
     })
